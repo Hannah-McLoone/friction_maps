799496d18f8c9007d22828e44217055ef6fe5490
Hannah McLoone <hm708@cam.ac.uk>
2025-05-05 13:20:35 +0100

    .

--- ccomparison_to_weiss_for_server.py
+++ ccomparison_to_weiss_for_server.py
@@ -16,56 +16,11 @@ use roads as mask
 """
 
 
-def calculate_speed(coords, params):
-    filename = '/maps/hm708/processed_land_without_speed'
-
-    df = pd.DataFrame({"pixel": [f"[{x}, {y}]" for (x, y) in coords]})
-    df['original_index'] = df.index
-
-    duckdb.query("DROP TABLE IF EXISTS input_pixels; CREATE TEMP TABLE input_pixels (pixel TEXT, original_index INTEGER)")
-    duckdb.query("INSERT INTO input_pixels SELECT pixel, original_index FROM df")
-
-    #edit this to be the actual parameter types used!!!
-    land_type_to_speed = {
-        "barren": params[0],          
-        "crop": params[1],        
-        "forest": params[2],            
-        "grass": params[3],          
-        "mangrove": params[4],         
-        "moss": params[5],              
-        "shrub": params[6],            
-        "snow": params[7],     
-        "urban": params[8],  
-        "wetland": params[9]   
-    }
-
-    # Convert the dictionary to a DataFrame
-    mapping_df = pd.DataFrame(list(land_type_to_speed.items()), columns=['land_type', 'speed'])
-
-    # Register it as a DuckDB table
-    duckdb.register('land_speed_mapping', mapping_df)
-
-    query = f"""
-    SELECT 
-        input_pixels.pixel, 
-        COALESCE(SUM(mapping.speed * read_parquet.coverage) / NULLIF(SUM(read_parquet.coverage), 0), 0) AS weighted_avg_speed
-    FROM input_pixels
-    LEFT JOIN read_parquet('{filename}*.parquet') AS read_parquet
-        ON input_pixels.pixel = read_parquet.pixel
-    LEFT JOIN land_speed_mapping AS mapping
-        ON read_parquet.subtype = mapping.land_type
-    GROUP BY input_pixels.pixel, input_pixels.original_index
-    ORDER BY input_pixels.original_index
-    """
-
-    result = duckdb.query(query).df()
-    return result['weighted_avg_speed']
-
-
 def create_table(coords):
     filename = '/maps/hm708/processed_land_without_speed'
+    cord_list = [f"[{x}, {y}]" for (x, y) in coords]
 
-    df = pd.DataFrame({"pixel": [f"[{x}, {y}]" for (x, y) in coords]})
+    df = pd.DataFrame({"pixel": cord_list})
     df['original_index'] = df.index
 
     duckdb.query("DROP TABLE IF EXISTS input_pixels; CREATE TEMP TABLE input_pixels (pixel TEXT, original_index INTEGER)")
@@ -77,60 +32,112 @@ def create_table(coords):
     FROM input_pixels
     LEFT JOIN read_parquet('{filename}*.parquet') AS read_parquet
     ON input_pixels.pixel = read_parquet.pixel
-    GROUP BY input_pixels.pixel, subtype
+    GROUP BY input_pixels.pixel, read_parquet.subtype
     """
 
     result = duckdb.query(query).df()
     pivot_df = result.pivot(index='pixel', columns='subtype', values='total_coverage').fillna(0)
-    print(pivot_df)
+    for col in ["barren", "crop", "forest", "grass", "mangrove", "moss", "shrub", "snow", "urban", "wetland"]:
+        if col not in pivot_df.columns:
+            pivot_df[col] = 0
+    
+    pivot_df.fillna(0, inplace=True)
+    pivot_df = pivot_df.loc[cord_list].reset_index()
+
+    return(pivot_df)
 
 
-def get_weiss_value(coords):
+def toblers_walking_speed(slope):
+    slope_rad = np.radians(slope)
+    adjusted_slope = np.abs(np.tan(slope_rad) + 0.05)
+    return 6 * np.exp(-3.5 * adjusted_slope)
+
+def elevation_adjustment(elevation):
+    return 1.016 * np.exp(-0.0001072 * elevation)
+
+
+
+def get_weiss_value(coords): # these are float coords
     values = []
+    resolution = 0.008333333333333333333
+
     with rasterio.open('2020_walking_only_friction_surface.geotiff') as src:
-        for (x,y) in coords:
-            row, col = src.index(x, y)
-            values.append(src.read(1)[row, col])
-            print('*')
-        return values
+        band1 = src.read(1)  # Read once instead of in the loop
+        index = src.index
+        for x, y in coords:
+            col, row = index(x * resolution, y * resolution)
+            values.append(band1[row, col])
+    
+    with rasterio.open('slope_1KMmd_SRTM.tif') as src:
+        band1 = src.read(1)  # Read once instead of in the loop
+        index = src.index
+        for i in range in range (0, len(coords)):
+            x,y = coords[i]
+            col, row = index(x * resolution, y * resolution)
+            slope_scaling = toblers_walking_speed(band1[row, col]) / 5
+            values[i] = values[i] / slope_scaling
 
+    with rasterio.open('elevation_1KMmd_SRTM.tif') as src:
+        band1 = src.read(1)  # Read once instead of in the loop
+        index = src.index
+        for i in range in range (0, len(coords)):
+            x,y = coords[i]
+            col, row = index(x * resolution, y * resolution)
+            elevation_scaling = elevation_adjustment(band1[row, col])
+            values[i] = values[i] / elevation_scaling
 
-#__________________________________________________________________
+def calculate_speed(coords, params):
+    #sum of speed * coverage / sum of coverage
+    print('-')
+    landtype_to_speed = {
+        "barren":params[0],
+        "crop": params[1],
+        "forest": params[2],
+        "grass": params[3],
+        "mangrove": params[4],
+        "moss": params[5],
+        "shrub": params[6],
+        "snow": params[7],
+        "urban": params[8],
+        "wetland": params[9]   
+    }
+
+    speed_series = pd.Series(landtype_to_speed)
+
+    # Select only land type columns
+    land_columns = list(landtype_to_speed.keys())
+
+    # Calculate weighted sum and total coverage
+    weighted_sum = table[land_columns].mul(speed_series)
+    coverage_sum = table[land_columns].sum(axis=1)
+    weighted_avg_speed = np.where(coverage_sum == 0, 0, weighted_sum.sum(axis=1) / coverage_sum)
+    return weighted_avg_speed
+    
+
 
 #pick coords between 85 and -60
-step = 0.008333333333333333333
-
-# Calculate number of steps for x and y
-x_steps = int((180 - (-180)) / step)
-y_steps = int((85 - (-60)) / step)
-
-# Generate 100 random step indices for x and y
+resolution = 0.008333333333333333333
 coordinates = []
 for _ in range(3):
-    x_index = random.randint(0, x_steps)
-    y_index = random.randint(0, y_steps)
-    
-    x = -180 + (x_index * step)
-    y = -60 + (y_index * step)  # Note: y should go from -60 to 85
-    
-    coordinates.append((x,y))
+    x = random.randint(-180, 180)
+    y = random.randint(-60, 85)# y should go from -60 to 85
+    coordinates.append((x//resolution, y//resolution))
 
 
-#__________________________________________________________
+#create table no longer in same order as coordinates
 
+truth = get_weiss_value(coordinates)
 
-#truth = get_weiss_value(coordinates)
-print("----")
 def residuals(params):
-    #sample = np.array(calculate_speed(coordinates, params))
-    return 1#sample - truth
+    sample = np.array(calculate_speed(coordinates, params))
+    return sample - truth
 
 
 # Initial guess: 30 parameters
 initial_guess = np.ones(10)
-create_table(coordinates)
-
-#result = least_squares(residuals, initial_guess, loss='linear', max_nfev=1)  # use 'soft_l1' for robustness
+table = create_table(coordinates)
+print('-')
+result = least_squares(residuals, initial_guess, loss='linear', max_nfev=3)  # use 'soft_l1' for robustness???
 
 print(result.x)
 with open('optimized_parameters.csv', 'w', newline='') as f:
