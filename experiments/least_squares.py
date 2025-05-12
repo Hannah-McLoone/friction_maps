d78e85412c9b3c1e3a3282d88396c7c02d731ef5
Hannah McLoone <hm708@cam.ac.uk>
2025-05-12 11:04:17 +0100

    improvements to optimisation

--- experiments/least_squares.py
+++ experiments/least_squares.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 
 def create_table(coords):
-    filename = '/maps/hm708/processed_land_without_speed'
+    filename = '/maps/hm708/backup/processed_land_without_speed'
     cord_list = [f"[{x}, {y}]" for (x, y) in coords]
 
     df = pd.DataFrame({"pixel": cord_list})
@@ -27,15 +27,17 @@ def create_table(coords):
     GROUP BY input_pixels.pixel, read_parquet.subtype
     """
 
-    result = duckdb.query(query).df()
-    result = result[result['subtype'] != 'land']
+    result = duckdb.query(query).df()# nan appears here
+    result = result[result['subtype'] != 'land'] 
     pivot_df = result.pivot(index='pixel', columns='subtype', values='total_coverage').fillna(0)
-    for col in ["barren", "crop", "forest", "grass", "mangrove", "moss", "shrub", "snow", "urban", "wetland","desert","glacier", "physical", "reef", "rock","sand","tree"]:
+    for col in ["barren", "crop", "forest", "grass", "mangrove", "moss", "shrub", "snow", "urban", "wetland"]:
         if col not in pivot_df.columns:
             pivot_df[col] = 0
+
     
     pivot_df.fillna(0, inplace=True)
     pivot_df = pivot_df.loc[cord_list].reset_index()
+
 
     return(pivot_df)
 
@@ -54,36 +56,18 @@ def get_weiss_value(coords): # these are float coords
     values = []
     resolution = 0.008333333333333333333
 
-    with rasterio.open('2020_walking_only_friction_surface.geotiff') as src:
+    with rasterio.open('/maps/hm708/2020_motorized_friction_surface.geotiff') as src:
         band1 = src.read(1)
         index = src.index
         for x, y in coords:
             col, row = index(x * resolution, y * resolution)
-            values.append(band1[col,row])#
+            values.append(band1[col,row])
 
-
-    with rasterio.open('/maps/hm708/slope_1KMmd_SRTM.tif') as src:
-        band1 = src.read(1)
-        index = src.index
-        for i in range(0, len(coords)):
-            x,y = coords[i]
-            col, row = index(x * resolution, y * resolution)
-            slope_scaling = toblers_walking_speed(band1[col, row]) / 5
-            values[i] = values[i] * slope_scaling
-
-    with rasterio.open('/maps/hm708/elevation_1KMmd_SRTM.tif') as src:
-        band1 = src.read(1)
-        index = src.index
-        for i in range(0, len(coords)):
-            x,y = coords[i]
-            col, row = index(x * resolution, y * resolution)
-            elevation_scaling = elevation_adjustment(band1[col,row])
-            values[i] = values[i] * elevation_scaling
     print('finished creating weiss values')
     return values
 
 
-def calculate_speed(table, params):
+def calculate_speed(table, params, scale):
     #sum of speed * coverage / sum of coverage
     landtype_to_speed = {
         "barren":params[0],
@@ -105,57 +89,86 @@ def calculate_speed(table, params):
     # Calculate weighted sum and total coverage
     weighted_sum = table[land_columns].mul(speed_series)
     coverage_sum = table[land_columns].sum(axis=1)
-    weighted_avg_speed = np.where(coverage_sum == 0, 0, weighted_sum.sum(axis=1) / coverage_sum)
-    weighted_avg_speed[weighted_avg_speed == 0] = 0.1
+    weighted_avg_speed = weighted_sum.sum(axis=1) / coverage_sum
     min_per_metre = 60 / (weighted_avg_speed* 1000)
+    min_per_metre = np.array(min_per_metre) / scale
     return min_per_metre
-    
+
 
 resolution = 0.008333333333333333
 coordinates = []
 
-# Generate 100,000 random coordinates
-for _ in range(5000000):
-    x = random.uniform(60,110)#(-179.9, 179.9)
-    y = random.uniform(40,60)#(-59.9, 84.9)
+
+
+
+for _ in range(1000000):
+    x = random.uniform(-179.9, 179.9)
+    y = random.uniform(-59.9, 59.9)
     coordinates.append((x // resolution, y // resolution))
-
-for _ in range(2000000):
-    x = random.uniform(100,125)#(-179.9, 179.9)
-    y = random.uniform(0,20)#(-59.9, 84.9)
-    coordinates.append((x // resolution, y // resolution))
-
-
-for _ in range(3000000):
-    x = random.uniform(-80,-50)#(-179.9, 179.9)
-    y = random.uniform(-10,10)#(-59.9, 84.9)
-    coordinates.append((x // resolution, y // resolution))
+#coordinates in terms of pixel coordinates, not degree coordinates
 
 
 
+table = create_table(coordinates)
+
+#filter out sea values, havent done sea travel
+rows_all_zero = (table.drop(columns='pixel') == 0).all(axis=1)
+table = table[~rows_all_zero].reset_index(drop=True)
+coordinates = [item for keep, item in zip(~rows_all_zero, coordinates) if keep]
 
 
 
-# Get the corresponding truth values
+scalings = []
+with rasterio.open('/maps/hm708/slope_1KMmd_SRTM.tif') as src:
+    band1 = src.read(1)
+    index = src.index
+    for i in range(0, len(coordinates)):
+        x,y = coordinates[i]
+        col, row = index(x * resolution, y * resolution)
+        slope_scaling = toblers_walking_speed(band1[col, row]) / 5
+        scalings.append(slope_scaling)
+
+with rasterio.open('/maps/hm708/elevation_1KMmd_SRTM.tif') as src:
+    band1 = src.read(1)
+    index = src.index
+    for i in range(0, len(coordinates)):
+        x,y = coordinates[i]
+        col, row = index(x * resolution, y * resolution)
+        elevation_scaling = elevation_adjustment(band1[col,row])
+        scalings[i] = scalings[i] * elevation_scaling
+
+scalings = np.array(scalings)
+
+
+
+
+
+
+
+
+
 truth = get_weiss_value(coordinates)
+# Get indices where values are greater than threshold
+indices = [i for i, v in enumerate(truth) if v > 0.01234]
+truth = [truth[i] for i in indices]
+scalings = [scalings[i] for i in indices]
+table = table.iloc[indices]
+
 
 # Split into train and test sets
-coords_train, coords_test, truth_train, truth_test = train_test_split(
-    coordinates, truth, test_size=0.2, random_state=42
+table_train, table_test, truth_train, truth_test, scalings_train, scalings_test = train_test_split(
+    table, truth, scalings, test_size=0.2, random_state=42
 )
+
+truth_train = np.array(truth_train)
 
 # Define residuals function using training data
 def residuals(params):
-    sample = np.array(calculate_speed(table_train, params))
-    return sample - truth_train
+    sample = np.array(calculate_speed(table_train, params, scalings_train))
+    return (sample - truth_train) / truth_train
 
 # Optimization
 initial_guess = np.ones(10)
-table_train = create_table(coords_train)
-table_test = create_table(coords_test)
-
-
-
 
 print('starting least squares')
 result = least_squares(residuals, initial_guess, loss='linear')
@@ -164,12 +177,12 @@ result = least_squares(residuals, initial_guess, loss='linear')
 print(result.x)
 
 # Optionally: Evaluate on test set
-predicted_test = calculate_speed(table_test, result.x)
+predicted_test = calculate_speed(table_test, result.x, scalings_test)
 
 df = pd.DataFrame({"predicted": predicted_test, "actual": truth_test})
-df.to_csv("predicted_vs_actual.csv", index=False)
+df.to_csv("predicted_vs_actual3.csv", index=False)
 
 error = np.array(predicted_test) - np.array(truth_test)
 print('Test RMSE:', np.sqrt(np.mean(error**2)))
 
-pd.Series(result.x).to_csv("optimized_parameters.csv", index=False, header=False)
+#pd.Series(result.x).to_csv("optimized_parameters.csv", index=False, header=False)
